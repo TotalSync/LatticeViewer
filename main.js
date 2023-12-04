d29a1a5dc8a623c1c5e96630394be667679ababd
IanGreen <Ian Green>
2023-12-04 16:06:58 -0600

    Attempting Integration of WebGL

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 
+const Bravias = {
+    Cubic: 0,
+    Tetragonal: 1,
+    Orthorhombic: 2,
+    Hexagonal: 3,
+    Rhombohedral: 4,
+    Monoclinic: 5,
+    Triclinic: 6,
+}
+
 
 class Config{
     constructor(){
@@ -7,20 +17,33 @@ class Config{
         this.win_width = 400;
         this.debug = false;
         this.matrix_stack = [];
+        this.bravais = Bravias.Cubic;
+        this.angles = [90.0, 90.0, 90.0];
+        this.lattice = new lattice();
     }
 }
+
+// Collect all the info needed to use the shader program.
+// Look up which attribute our shader program is using
+// for aVertexPosition and look up uniform locations.
+const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(cfg.shaderProgram, "aVertexPosition"),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(cfg.shaderProgram, "uProjectionMatrix"),
+      modelViewMatrix: gl.getUniformLocation(cfg.shaderProgram, "uModelViewMatrix"),
+    },
+  };
+  
 
 class vertex
 {
     constructor(pos)
     {   
         this.pos = pos;
-    }
-    constructor(x,y,z)
-    {
-        this.pos = new pos(x,y,z);
-    }
-     
+    }     
 }
 
 class pos
@@ -32,59 +55,135 @@ class pos
         this.z = z;
     }
 }
-class mat3
+class lattice
 {
-    constructor()
+    constructor(a=1, b=1, c=1)
     {
-        this.e =   [1,0,0,
-                    0,1,0,
-                    0,0,1];
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
-    constructor(mat){ this.e = mat; }
-}
 
-class mat4
-{
-    constructor()
-    {
-        this.e =   [1,0,0,0,
-                    0,1,0,0,
-                    0,0,1,0,
-                    0,0,0,1];
-    }
-    constructor(mat){ this.e = mat; }
 }
 
 var cfg = new Config();
 
+function loadInitialSettings()
+{
+    cfg.matrix_stack[0] = proj_mat(90,cfg.win_width/cfg.win_width, 0, 100);
+    cfg.vert = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    cfg.frag = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    cfg.shaderProgram = gl.createProgram();
+    gl.attachShader(cfg.shaderProgram, cfg.vert);
+    gl.attachShader(cfg.shaderProgram, cfg.frag);
+    gl.linkProgram(cfg.shaderProgram);
+
+    if (!gl.getProgramParameter(cfg.shaderProgram, gl.LINK_STATUS)) {
+        alert(
+          `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+            shaderProgram,
+          )}`,
+        );
+      }
+
+}
+
 function main(cfg)
 {
     cfg.canvas = document.getElementById("world");
-    
+    const gl = cfg.canvas.getContext('webgl');
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    if (cfg.canvas.getContext)
+    {
+        if(gl)
+        {
+            // Here's where we call the routine that builds all the
+            // objects we'll be drawing.
+            const buffers = initBuffers(gl);
+
+            // Draw the scene
+            drawScene(gl, programInfo, buffers);
+        } 
+        updateWorld();
+    }
 }
 
-function update_world()
+function updateWorld()
 {
-    let ctx = cfg.canvas.getContext('2d');
+    let ctx = cfg.canvas.getContext('webgl');
+
+    window.requestAnimationFrame(updateWorld);
 }
 
-
-
-
-// Matrix Math Functions
-
-// Returns a projection matrix based on the given fov
-function proj_mat(fov, aspect, near, far)
+function render(cfg, ctx)
 {
-    var top, bot, left, right;
-    top = near * Math.tan(toRadians(fov)/2);
-    bot = -top;
-    right = top * aspect;
-    left = -right;
-
-    var proj = [2*near/(right-left), 0,0, -near*(right+left)/(right-left),
-                0,2*near/(top-bot),0,-near*(top+bot)/(top-bot),
-                0,0,-(far+near)/(far-near),2*far*near/(near-far),
-                0,0,-1,0];
-    return mat4(proj);
+    cfg.matrix_stack.forEach(mat => {
+        
+    });
 }
+
+function resizeCanvas()
+{
+   cfg.canvas = document.getElementById("world");
+   let offset = document.getElementById("controls");
+   //let offset = 1;
+   // Set the dimensions of the window. 30, and 20 are window scalars since window does not fit in box.
+   cfg.win_width = cfg.canvas.width = window.innerWidth - 30;
+   cfg.win_height = cfg.canvas.height = window.innerHeight - 20 - offset.clientHeight;
+}
+
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+  
+    // Send the source to the shader object
+  
+    gl.shaderSource(shader, source);
+  
+    // Compile the shader program
+  
+    gl.compileShader(shader);
+  
+    // See if it compiled successfully
+  
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(
+        `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
+      );
+      gl.deleteShader(shader);
+      return null;
+    }
+  
+    return shader;
+  }
+
+const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+// Vertex shader program
+const vsSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+
+
+import { initBuffers } from "./init_buffers.js";
+import { drawScene } from "./draw_scene.js";
+
+window.onload = function ()
+{
+   window.addEventListener("resize", resizeCanvas, false);
+   //window.requestAnimationFrame(UpdateWorld);
+   loadInitialSettings();
+   resizeCanvas();
+   main(cfg);
+};
